f655710ffe5b302b051849f56153989c030ee1b1
Adaora Asika <adaoraasika@gmail.com>
2025-02-22 13:44:51 +0000

    added fact_quart_rev table

--- 4-Analytics_Engineering/analytics_engineering/taxi_rides_ny/models/core/fct_taxi_trips_quarterly_revenue.sql
+++ 4-Analytics_Engineering/analytics_engineering/taxi_rides_ny/models/core/fct_taxi_trips_quarterly_revenue.sql
@@ -1,13 +1,17 @@
-{{ config(materialized="table") }}
-
-with trips_data as (select * from {{ ref("fact_trips") }})
-
-with
-    quarterly_revenue as (
-        select service_type, year, quarter, year_quarter, sum(total_amount) as revenue
-        from trips_data
-        group by 2, 3, 4
+{{
+    config(
+        materialized='table'
     )
+}}
+
+with trips_data as (
+    select * from {{ ref("fact_trips") }}
+    ),
+quarterly_revenue as (
+    select service_type, year, quarter, year_quarter, sum(total_amount) as revenue
+    from trips_data
+    group by 1,2, 3, 4
+)
 select
     q1.service_type as service_type,
     q1.year as year,
@@ -16,7 +20,7 @@ select
     q1.revenue as curr_year_rev,
     q2.revenue as last_year_rev,
     round(
-        (q1.curr_year_rev - q2.last_year_rev) / q2.last_year_rev * 100, 2
+        (q1.revenue - q2.revenue) / q2.revenue * 100, 2
     ) as yoy_rev_growth_percentage
 from quarterly_revenue q1
 left join quarterly_revenue q2 on q1.quarter = q2.quarter and q1.year = q2.year + 1
